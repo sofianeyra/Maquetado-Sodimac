4039b76286b919bfd6cdcd2ae45d4012c701e195
sofianeyra <safirokpoper@gmail.com>
2019-09-02 17:51:48 -0500

    Añadiendo componente categoryBar a App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Topbar from './components/topBar';
 import NavBar from './components/navBar';
+import CategoryBar from './components/categoryBar';
 function App() {
   return (
     <div className="App">
         <Topbar/>
         <NavBar/>
+        <CategoryBar/>
     </div>
   );
 }
