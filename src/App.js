39c01da336a96453a12465c68dcb74899070472b
sofianeyra <safirokpoper@gmail.com>
2019-09-02 16:32:36 -0500

    Añdiendo componente navBar a App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Topbar from './components/topBar';
+import NavBar from './components/navBar';
 function App() {
   return (
     <div className="App">
         <Topbar/>
+        <NavBar/>
     </div>
   );
 }
