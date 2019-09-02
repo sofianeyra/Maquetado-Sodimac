7c3d8c3e1ef75a7220ba88e7b0a29a17bf65a4b0
sofianeyra <safirokpoper@gmail.com>
2019-09-02 17:46:14 -0500

    corrección de classnames en componente navBar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -13,17 +13,24 @@ class NavBar extends Component {
         return(
             <section className="main-header">
                 <div className="brand-section">
-                    <img src={mainLogo} alt="main-logo"></img>
-                    <p>
-		                <a className="bold-text font-size-11" title="Sodimac Homecenter" href="https://www.sodimac.com.pe/sodimac-pe">Homecenter</a> | <a className="bold-text font-size-11" title="Sodimac Constructor" href="https://www.sodimac.com.pe/sodimac-pe/sodimac-constructor">Constructor</a>
+                    <p className="navbar">
+                        <img className="main-logo" src={mainLogo} alt="main-logo"></img>
+                        <span className="p-15"></span>
                         <input className="search-box" placeholder="¿Qué estás buscando?" name="Ntt" value=""></input>
-                        <img className="gps-icon" src={gps} alt="location"></img>
+                        <div className="right-section">
+                            <img className="gps-icon" src={gps} alt="location"></img>
                         LIMA
-                        <span className="counter">1</span>
-                        <img className="shopping-icon" src={shopping} alt="shopping cart"></img>
-                        <img className="user-icon" src={user} alt="user"></img>
-                        <p>Mi cuenta</p>
+                        <span className="p-20"></span>
+                            <button type="button" className="counter">1</button>
+                            <img className="shopping-icon" src={shopping} alt="shopping cart"></img>
+                            <span className="p-20"></span>
+                            <img className="user-icon" src={user} alt="user"></img>
+                        Mi cuenta
+                        </div>
                     </p>
+                    <div className="brands-bar">
+                    <a className="brands left-side bold-text font-size-11" title="Sodimac Homecenter" href="https://www.sodimac.com.pe/sodimac-pe">Homecenter</a> | <a className="brands bold-text font-size-11" title="Sodimac Constructor" href="https://www.sodimac.com.pe/sodimac-pe/sodimac-constructor">Constructor</a>
+                    </div>
                 </div>
             </section>
         )
